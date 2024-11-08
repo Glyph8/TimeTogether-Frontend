6f06af1a286a56185a12f9ec5050a35924dcc6f8
nayoung <nayoung021008@naver.com>
2024-11-08 16:26:01 +0900

    캘린더 레이아웃

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,46 +1,6 @@
-// import React, { useEffect, useState } from "react";
-// import { Link } from "react-router-dom";
-// import "./Login.css"; // css파일 import
-// import logo from "../img/logo.png"; // 이미지 가져오기
-// import logo2 from "../img/logo_purple.png";
-// import kakao from "../img/kakao.png";
-// import naver from "../img/naver.png";
-// import google from "../img/google.png";
-
-// function Login() {
-//   return (
-//     <div className="login-container">
-//       {/* 로고 */}
-//       <Link to="/group">
-//         <img className="login-logo" src={logo} alt="로고"></img>
-//       </Link>
-//       {/* 앱 이름 */}
-//       <h1 className="login-title">타임투게더</h1>
-
-//       {/* 앱 설명 */}
-//       <p className="login-description">쉽고 빠른 약속 생성 도우미</p>
-
-//       {/* 소셜 로그인 */}
-//       <div className="social-login-container">
-//         <p className="social-login-title">소셜 로그인</p>
-//         <a href="/oauth2/authorization/naver">
-//           <img className="social-button" src={naver} alt="네이버로 시작"></img>
-//         </a>
-//         <a href="/oauth2/authorization/kakao">
-//           <img className="social-button" src={kakao} alt="카카오로 시작"></img>
-//         </a>
-//         <a href="/oauth2/authorization/google">
-//           <img className="social-button" src={google} alt="구글로 시작"></img>
-//         </a>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Login;
-
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import "./Login.css";
 import logo from "../img/logo.png";
 import kakao from "../img/kakao.png";
@@ -48,52 +8,82 @@ import naver from "../img/naver.png";
 import google from "../img/google.png";
 
 function Login() {
-  const handleLogin = async (provider) => {
-    try {
-      const response = await fetch(
-        `http://192.168.86.49:8080/oauth2/authorization/${provider}`,
-        {
-          method: "GET",
-          credentials: "include",
-        }
-      );
+  const navigate = useNavigate();
 
-      if (response.ok) {
-        // 헤더에서 토큰 추출
-        const token = response.headers.get("Authorization"); // 토큰을 담고 있는 헤더 키로 수정
-        if (token) {
-          localStorage.setItem("token", token); // 로컬 스토리지에 저장
-          window.location.href = "/group"; // 로그인 성공 시 리다이렉트
-        } else {
-          console.error("토큰을 찾을 수 없습니다.");
-        }
-      } else {
-        console.error("로그인 실패");
-      }
-    } catch (error) {
-      console.error("로그인 요청 중 오류 발생:", error);
-    }
-  };
+  // 카카오 인가 요청 URL 생성
+  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=f0cfd8fd7bb4da57b220043a25208ca7&redirect_uri=http://172.20.10.4:3000/oauth2/redirect&response_type=code`;
+
+  // useEffect(() => {
+  //   console.log("1111111");
+  //   // URL에서 code 파라미터 추출
+  //   const urlParams = new URLSearchParams(window.location.search);
+  //   const authCode = urlParams.get("code");
+
+  //   console.log("2222222");
+  //   if (authCode) {
+  //     console.log("333333");
+  //     // 인가 코드가 존재할 경우 alert로 출력
+  //     window.alert(`인가 코드 받음: ${authCode}`);
+  //     console.log("444444");
+  //     // 백엔드에 POST 요청으로 인가 코드 전달하여 토큰 받기
+  //     axios
+  //       .post("http://172.20.10.9:8080/oauth2/authorize/kakao", {
+  //         code: authCode,
+  //       })
+  //       .then((response) => {
+  //         const { token } = response.data;
+  //         localStorage.setItem("token", token); // 토큰을 로컬 스토리지에 저장
+  //         navigate("/group"); // 메인 페이지로 리디렉트
+  //       })
+  //       .catch((error) => {
+  //         console.error("로그인 실패:", error);
+  //       });
+  //   } else {
+  //     console.log("555555");
+  //     // 인가 코드가 없을 경우 alert로 "안 뜸" 출력
+  //     window.alert("인가 코드 없음");
+  //   }
+  //   console.log("666666");
+  // }, []);
 
   return (
     <div className="login-container">
-      <Link to="/group">
+      <a href="/group">
+        {/* 일단 로고 누르면 다음으로 넘어가게함 */}
         <img className="login-logo" src={logo} alt="로고" />
-      </Link>
+      </a>
       <h1 className="login-title">Time Together</h1>
       <p className="login-description">쉽고 빠른 약속 생성 도우미</p>
 
       <div className="social-login-container">
         <p className="social-login-title">소셜 로그인</p>
-        <button onClick={() => handleLogin("naver")}>
-          <img className="social-button" src={naver} alt="네이버로 시작" />
-        </button>
-        <button onClick={() => handleLogin("kakao")}>
+
+        {/* 카카오 로그인 버튼 */}
+        <a
+          href="http://172.20.10.9:8080/oauth2/authorization/kakao"
+          target="_self"
+          rel="noopener noreferrer"
+        >
           <img className="social-button" src={kakao} alt="카카오로 시작" />
-        </button>
-        <button onClick={() => handleLogin("google")}>
+        </a>
+
+        {/* 네이버 로그인 버튼 */}
+        <a
+          href="http://172.20.10.9:8080/oauth2/authorization/naver"
+          target="_self"
+          rel="noopener noreferrer"
+        >
+          <img className="social-button" src={naver} alt="네이버로 시작" />
+        </a>
+
+        {/* 구글 로그인 버튼 */}
+        <a
+          href="http://172.20.10.9:3000/oauth2/authorization/google"
+          target="_self"
+          rel="noopener noreferrer"
+        >
           <img className="social-button" src={google} alt="구글로 시작" />
-        </button>
+        </a>
       </div>
     </div>
   );
